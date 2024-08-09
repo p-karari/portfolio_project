815ed69ee346c3a78ffcb6a841180543a621ae6c
p.karari <kararikelvin2002@gmail.com>
2024-08-09 16:06:37 +0300

    Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,103 @@
+
+function Projects() {
+  return (
+    <>
+<div className="relative mx-auto text-4xl font-bold w-[50%] my-[2rem] flex items-center justify-center">
+  <span className="absolute text-[6rem] text-gray-400 opacity-30 ">Projects</span>
+  <p className="relative z-10 mt-[1rem]">My Projects</p>
+</div>
+    <div className='flex flex-row mx-auto w-[80%] my-[1rem]'>
+        <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div> 
+
+        <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div>
+
+
+    </div>
+    <div className='flex flex-row mx-auto w-[80%] my-[1rem]'>
+
+    <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='border border-red-700 w-[19rem] h-[30rem] m-auto '>
+          <div className="w-[100%] border border-green-800 h-[55%]"></div>
+          <div className="h-[40%] m-[5px]">
+            <h1 className="text-2xl font-bold m-auto w-[60%]">Project name</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+               Dolor alias quo odit sunt rerum suscipit praesentium, quidem doloremque sequi architecto temporibus vero, quaerat ab tempore.
+            </p>
+            <div className="flex flex-row my-[5px]">
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Github</button>
+            <button className="w-[45%] p-[8px] rounded-lg mx-[1rem] border border-grey-900">Visit</button>
+            </div>
+          </div>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Projects
