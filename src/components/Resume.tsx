3e267433972c6e236291cbb4cc01e989f0352e66
p.karari <kararikelvin2002@gmail.com>
2024-08-10 16:07:50 +0300

    Resume page

---
+++ src/components/Resume.tsx
@@ -0,0 +1,64 @@
+
+function Resume() {
+  return (
+    <>
+<div className="relative mx-auto text-4xl font-bold w-[50%] my-[2rem] flex items-center justify-center">
+  <span className="absolute text-[6rem] text-gray-400 opacity-30 ">Mini-Timeline</span>
+  <p className="relative z-10 mt-[1rem]">Resume</p>
+</div>
+    <div className="flex flex-row mx-auto w-[85%]">
+        <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+          <div><h1 className="text-2xl text-blue-500 font-bold capitalize">June 2024 - August 2024</h1></div>
+          <div><h2 className="text-1.4xl text-gray-700 font-bold ">Teach2Give attachee</h2></div>
+          <div><h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">Teach2Give</span></h3></div>
+          <p>Participated in a three month cohort for fullstack development by Teach2Give.
+           Teach2Give is a non-profit organisation whose main goal is to mentor youth in the tech field and equip them with the necessary skills to succeed in the field.</p>
+        </div>
+        <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+          <h1 className="text-2xl text-blue-500 font-bold capitalize">June 2023 - August 2023</h1>
+          <h2 className="text-1.4xl text-gray-700 font-bold ">Power Learn Project attachee</h2>
+          <h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">Teach2Give</span></h3>
+          <p>Participated in a four month intensive training programme by Power Learn Project organisation.Power Learn Project equips Africa's youth with vital digital skills through their fully funded 16-week software development program,
+        driving sustainable development and creating pathways to meaningful employment and economic growth.</p>
+       </div>
+    </div>
+
+    <div className="flex flex-row mx-auto w-[85%]">
+    <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+    <h1 className="text-2xl text-blue-500 font-bold capitalize">Jan 2022 - Current</h1>
+          <h2 className="text-1.4xl text-gray-700 font-bold ">Computer Society of Kirinyaga</h2>
+          <h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">C.S.K, Kirinyaga University</span></h3>
+        <p>Joined Computer society of Kirinyaga as a mentee in their Web-development mentorship programme
+        where i built and deployed my first ever website and have been in love with the craft since.Since then i have accrued enough experience and i'm now a mentor in the programme</p>
+        </div>
+        <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+        <h1 className="text-2xl text-blue-500 font-bold capitalize">Septemeber 2021 - Current</h1>
+          <h2 className="text-1.4xl text-gray-700 font-bold ">Bsc Software Engineering</h2>
+          <h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">Kirinyaga University</span></h3>
+        <p>Pursuing a Bachelor's degree in Software Engineering at Kirinyaga University, Kirinyaga.
+        Started in September 2021, with an expected completion by September 2025.</p>
+        </div>
+    </div>
+
+    <div className="flex flex-row mx-auto w-[85%]">
+    <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+    <h1 className="text-2xl text-blue-500 font-bold capitalize">Jan 2017 - April 2021</h1>
+          <h2 className="text-1.4xl text-gray-700 font-bold ">K.C.S.E</h2>
+          <h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">Kijabe Boys Highschool</span></h3>
+          <p>Attended Kijabe Boys High School from 2017 to 2021, where I completed my secondary education. Scored a B plain in the Kenya Certificate of Secondary Education (KCSE),
+             averaging a grade of B across all subjects.</p>
+        </div>
+        <div className="border border-black-700 w-[45%] mx-[2rem] my-[1rem] h-[17rem] flex flex-col gap-4 p-[10px] bg-gray-100 shadow-2xl rounded">
+        <h1 className="text-2xl text-blue-500 font-bold capitalize">Jan 2007 - 2016</h1>
+          <h2 className="text-1.4xl text-gray-700 font-bold ">K.C.P.E</h2>
+          <h3 className="text-1.2xl text-gray-700 font-bold capitalize border-l-[3.5px] border-l-gray-800 mx-[0.2rem]"><span className="mx-[0.3rem]">serene academy</span></h3>
+
+        <p>I attended Serene Academy from 2008 to 2016, completing my primary school 
+        education with distinction and achieving exceptional results in my KCPE exams with a straight A in mathematics</p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Resume
