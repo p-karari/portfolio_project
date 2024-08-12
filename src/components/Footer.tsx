bc902916058e722c0fbc0e51f7ddd59ebacd851f
p.karari <kararikelvin2002@gmail.com>
2024-08-12 16:18:35 +0300

    Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import Icons from "./Icons"
+function Footer() {
+  return (
+
+<>
+<footer className="footer footer-center bg-base-300 text-base-content p-4">
+  <aside>
+    <p>Developed by <span className="text-1xl font-bold text-white border border-blue-500 rounded-xl p-[5px] mx-auto bg-blue-500">Kelvin Karari</span></p>
+    <p>Thankyou for taking the time to go through my website. I l❤️ve you</p>
+  </aside>
+    <Icons />
+</footer>
+</>
+  )
+}
+
+export default Footer
