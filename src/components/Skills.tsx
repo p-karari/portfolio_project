fdb71898b2bac2cf4909d6c4f5ef9b8ed967491c
p.karari <kararikelvin2002@gmail.com>
2024-08-28 09:39:59 +0300

    Modified skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,13 +6,18 @@ import oop from '../images/object oriented.jpg'
 import designTools from '../images/design tools icon.jpg'
 import analysis from '../images/analysis icon.jpg'
 import dbicon from '../images/db1.jpg'
+import FastApiPic from '../images/FastApiPic.png'
+import DrizzlePic from '../images/DrizzlePic.jpg'
+import HonoPic from '../images/HonoPic.png'
+import DjangoPic from '../images/download.png'
+
 
 function skills() {
   return (
     <>
-
-<div className="relative mx-auto text-4xl font-bold w-[50%] my-[2rem] flex items-center justify-center">
-  <span className="absolute text-[6rem] text-gray-400 opacity-30 ">skills repository</span>
+    <div>
+<div className="relative mx-auto text-4xl font-bold w-[50%] flex items-center justify-center">
+  <span className="absolute text-[4rem] text-gray-400 opacity-30 ">skills repository</span>
   <p className="relative z-10 mt-[1rem]">My Skills</p>
 </div>
 
@@ -48,34 +53,89 @@ function skills() {
         <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
             <figure>
                 <img
-                src={npm}
-                alt="npm icon" />
+                src={DrizzlePic}
+                alt="react icon" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">npm !</h2>
-                <p>Proficient in using npm for managing project dependencies, handling packages, and scripts, and ensuring smooth 
-                    development workflows to enhance project maintainability and collaboration.</p>
+                <h2 className="card-title">Drizzle</h2>
+                <p>Experienced with Drizzle for efficient and type-safe database management. Proficient in schema migrations, query optimization, 
+                    and integrating Drizzle with modern web frameworks.</p>
             </div>
         </div>
 
         <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
             <figure>
                 <img
-                src={debugging}
-                alt="debugging icon" />
+                src={HonoPic}
+                alt="react icon" />
             </figure>
             <div className="card-body">
-                <h2 className="card-title">Debugging</h2>
-                <p>Effective problem-solver with a keen eye for detail, utilizing debugging tools and techniques to identify and resolve code issues quickly,
-                     ensuring stable and reliable applications.</p>
+                <h2 className="card-title">Hono</h2>
+                <p>Proficient in Hono for building fast, lightweight web APIs. Skilled in middleware handling, request routing, 
+                    and optimizing performance for modern web applications.</p>
             </div>
         </div>
 
     </div>
 
     <div className="flex flex-row w-[80%] mx-auto my-[1rem]">
+        <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
+        <figure>
+            <img
+            src={DjangoPic}
+            alt="react icon" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">Django</h2>
+            <p>Skilled in Django for building robust, scalable web applications. Proficient in the MTV architecture, ORM,
+                 and authentication to develop secure, high-performance backends.</p>
+        </div>
+        </div>
 
-    
+        <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
+        <figure>
+            <img
+            src={FastApiPic}
+            alt="react icon" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">FastApi</h2>
+            <p>Skilled in FastAPI for creating high-performance APIs with automatic documentation. Experienced in async handling,
+                 request validation, and seamless integration with databases.</p>
+        </div>
+        </div>
+
+        <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
+        <figure>
+            <img
+            src={npm}
+            alt="npm icon" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">npm !</h2>
+            <p>Proficient in using npm for managing project dependencies, handling packages, and scripts, and ensuring smooth 
+                development workflows to enhance project maintainability and collaboration.</p>
+        </div>
+        </div>
+
+        <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
+        <figure>
+            <img
+            src={debugging}
+            alt="debugging icon" />
+        </figure>
+        <div className="card-body">
+            <h2 className="card-title">Debugging</h2>
+            <p>Effective problem-solver with a keen eye for detail, utilizing debugging tools and techniques to identify and resolve code issues quickly,
+                ensuring stable and reliable applications.</p>
+        </div>
+        </div>
+
+
+    </div>
+
+    <div className="flex flex-row w-[80%] mx-auto my-[1rem]">
+        
     <div className="card bg-base-100 image-full w-96 shadow-xl mx-[10px]">
             <figure>
                 <img
@@ -129,8 +189,12 @@ function skills() {
         </div>
 
     </div>
+    </div>
     </>
   )
 }
 
 export default skills
+
+
+
