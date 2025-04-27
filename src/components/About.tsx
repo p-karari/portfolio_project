ed9255d16d73c661eeb52e9267df83fa7e263614
p.karari <kararikelvin2002@gmail.com>
2025-04-27 21:05:05 +0300

    Updated font and styling on the about page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,17 @@
 import curious from '../images/curious.jpg'
-import Bg2 from '../images/bg2.jpg'
+import { Tilt } from 'react-tilt'
 function About() {
   return (
     <>
-    <div className="h-[35rem] hero bg-cover bg-center bg-no-repeat flex flex-col" style={{ backgroundImage: `url(${Bg2})` }}>
+    <div className="h-[35rem] hero flex flex-col">
 
       <div className="relative mx-auto text-4xl font-bold w-[50%] my-[2rem] flex items-center justify-center">
-          <span className="absolute text-[6rem] text-gray-600 opacity-70 ">About</span>
-          <p className="relative z-10 mt-[1rem]">About Me</p>
+          <span className="absolute text-[6rem] text-gray-600 opacity-70 font-general text-gold">About</span>
+          <p className="relative z-10 mt-[1rem] font-general text-white">About Me</p>
       </div>   
        <div className="flex flex-row mx-auto w-[80%]">
-          <div className="w-[30rem] h-[15rem] mx-auto my-[1rem]"><img className='rounded-lg shadow-2xl' src={curious} alt="curious icon.jpg" /></div>
-          <div className="w-[25rem] h-[22rem] mx-auto my-[1rem] text-1.3xl text-grey">
+          <div className="w-[30rem] h-[15rem] mx-auto my-[1rem]"> <Tilt options={{ max: 25, scale: 1.05 }}> <img className='rounded-lg shadow-2xl' src={curious} alt="curious icon.jpg" /> </Tilt></div>
+          <div className="w-[25rem] h-[22rem] mx-auto my-[1rem] text-1.3xl text-white font-general">
             My name is Kelvin Karari or you can call me dev Karari. I am a kirinyaga university graduate with a bachelor's degree in software engineering.
             I am a plp certified fullstack developer, my technical skills include proficiency in typescript, drizzle orm, hono and postgresql for backend, React js, tailwind,material ui, html and css are my preffered frontend languages.
             I've built and deployed complex applications with these technologies. I take pride in what i do, i am a team player and thrive in collaborative environments. I pay attention to details in any solution i'm working on and find creative ways to solve problems.
